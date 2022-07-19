831bf252f58b2c9187e0e07d712476be374c9a8f
mr_kim <rofalfl42@gmail.com>
2022-07-19 23:22:04 +0900

    [작성자] 김미리 [문제] 백준 1253번 좋은 수 구하기 [수정사항] 문제 클래스, 답만 추가.. 뇌가 안돌아간다아아

--- src/no7/No7JooMong.java
+++ src/no7/No7JooMong.java
@@ -3,7 +3,7 @@ package no7;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class No6JooMong {
+public class No7JooMong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.nextLine());
---
+++ src/no8/No8GoodNumber.java
@@ -0,0 +1,7 @@
+package no8;
+
+public class No8GoodNumber {
+    public static void main(String[] args) {
+
+    }
+}
