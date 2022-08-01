7ce13809e3117cfd01a2277adc383b9563a19470
mr_kim <rofalfl42@gmail.com>
2022-08-01 23:34:36 +0900

    [문제] 백준 1874번 [수정사항] 스택 수열 문제 푸는 중..

---
+++ src/no11/No11Stack.java
@@ -0,0 +1,39 @@
+package no11;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+public class No11Stack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        System.out.println(functionStack(N, A));
+    }
+
+    public static StringBuilder functionStack(int N, int[] A) {
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder answer = new StringBuilder();
+        int num = 1;
+        for (int i = 0; i < N; i++) {
+            if (A[i] >= num) {
+                while (A[i] >= num) {
+                    stack.push(num++);
+                    answer.append("+\n");
+                }
+                stack.pop();
+                answer.append("-\n");
+            } else if (A[i] < i) {
+                int su = stack.pop();
+                if (su > A[i]) return new StringBuilder("NO");
+                else answer.append("-\n");
+            }
+        }
+        return answer;
+    }
+}
