1ed6dfe98af7d3cfa26aa9bee748c661074faa2a
mr_kim <rofalfl42@gmail.com>
2022-11-06 23:31:00 +0900

    [수정사항] 코드트리 기본 문법 (자바의 입출력) 복습 중

---
+++ src/codetree/input/Main.java
@@ -0,0 +1,49 @@
+package codetree.input;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        - 입출력 -
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(a + 2);
+
+        - 정수 -
+
+        1. 점수 입력
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        System.out.printf("Your score is %d point.", score);
+
+        2. 정수 입력받아 계산
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print(a*2);
+
+        3. 정수 입력받아 계산
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print((2 * a) + 3);
+
+        - 실수 -
+        1. 실수 입력
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("%.2f",a);
+
+        2. 길이 단위 환산하기
+        Scanner sc = new Scanner(System.in);
+        double ft = sc.nextDouble();
+        double cm = ft * 30.48;
+        System.out.printf("%.1f", cm);
+
+        3. 실수 입력받아 계산
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("%.2f", 1.5 + a);
+
+        
+
+         */
+    }
+}
