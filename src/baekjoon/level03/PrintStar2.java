e6920981d459aa2adb276b6ed85f7002c6308d58
mr_kim <rofalfl42@gmail.com>
2022-11-14 23:28:00 +0900

    [BaekJoon] 백준 단계별 문제 풀이 중 3단계(반복문) 2439 문제

---
+++ src/baekjoon/level03/PrintStar2.java
@@ -0,0 +1,46 @@
+package baekjoon.level03;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PrintStar2 {
+    public static void printResult() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            int count = System.in.read();
+            System.out.println(count);
+            count -= '0';
+            System.out.println(count);
+            for (int i = count, j = 1; i > 0; i--) {
+                sb.append("*".repeat(j++)).append("\n");
+            }
+            System.out.println(sb);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void printShortResult() {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= count; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= count; i++) {
+            sb.append(" ".repeat(count - i));
+            sb.append("*".repeat(i));
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
