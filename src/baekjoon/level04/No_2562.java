121ef6f3ec41f008cf2fc95cec2de5d4842e6747
mr_kim <rofalfl42@gmail.com>
2022-11-29 23:36:57 +0900

    [BaekJoon] 백준 단계별 문제 풀이 중 4단계(1차원 배열 ~ 4)

--- src/baekjoon/level04/No_2562.java
+++ src/baekjoon/level04/No_2562.java
@@ -1,2 +1,28 @@
-package baekjoon.level04;public class No_2562 {
+package baekjoon.level04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_2562 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int max = 0;
+            int index = 0;
+            for (int i = 0; i < 9; i++) {
+                int num = Integer.parseInt(br.readLine());
+                if (max < num) {
+                    max = num;
+                    index = i;
+                }
+            }
+
+            System.out.println(max);
+            System.out.println(index + 1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
