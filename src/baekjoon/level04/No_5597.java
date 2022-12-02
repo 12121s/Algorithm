2ce68ca2f72eb8cfd0a4e8f2cb87b8fbddd946d8
mr_kim <rofalfl42@gmail.com>
2022-12-03 00:22:47 +0900

    [BaekJoon] 백준 단계별 문제 풀이 중 4단계(1차원 배열 ~ 5)

--- src/baekjoon/level04/No_5597.java
+++ src/baekjoon/level04/No_5597.java
@@ -1,63 +1,27 @@
 package baekjoon.level04;
 
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class No_10871 {
-    public static void printNFasterResult() {
-        // 	memory: 18024(KB), time: 212(ms)
+public class No_5597 {
+    public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            List<String> arrList = IntStream.rangeClosed(1, 30)
+                    .mapToObj(Integer::toString)
+                    .collect(Collectors.toList());
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int n = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-
-            int[] array = new int[n];
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                array[i] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < 28; i++) {
+                arrList.remove(br.readLine());
             }
 
-            for (int i = 0; i < n; i++) {
-                if (array[i] < x) bw.write(array[i] + " ");
+            for (String s : arrList) {
+                System.out.println(s);
             }
-            bw.flush();
-
-            br.close();
-            bw.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void printMFasterResult() {
-        // 	memory: 15632(KB), time: 172(ms)
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int n = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-
-            int[] array = new int[n]; // 사실 로직적으로 배열 생성 안하고 입력받는 값마다 x보다 작은지 비교하는 로직이 더 빠름.
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                array[i] = Integer.parseInt(st.nextToken());
-            }
-
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < n; i++) {
-                if (array[i] < x) sb.append(array[i]).append(' ');
-            }
-
-            System.out.println(sb);
 
             br.close();
         } catch (IOException e) {
