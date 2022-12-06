0eee770812bbadf5493a5c4040e52be40207d28d
mr_kim <rofalfl42@gmail.com>
2022-12-06 21:36:26 +0900

    [Programmers] 입문 문제 외계어 사전 완료

--- src/programmers/intro/Solution_alienLangDictionary.java
+++ src/programmers/intro/Solution_alienLangDictionary.java
@@ -1,2 +1,20 @@
-package programmers.intro;public class Solution_alienLangDictionary {
+package programmers.intro;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class Solution_alienLangDictionary {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"p", "o", "s"}, new String[]{"sod", "eocd", "qixm", "adio", "soo"}));
+        System.out.println(solution(new String[]{"z", "d", "x"}, new String[]{"def", "dww", "dzx", "loveaw"}));
+        System.out.println(solution(new String[]{"s", "o", "m", "d"}, new String[]{"moos", "dzx", "smm", "sunmmo", "som"}));
+        System.out.println(solution(new String[]{"s", "o", "m"}, new String[]{"moos", "dzx", "smm", "sunmmo", "som"}));
+    }
+
+    public static int solution(String[] spell, String[] dic) {
+        return Arrays.stream(dic)
+                .anyMatch(s -> Arrays.stream(spell)
+                        .allMatch(s::contains))? 1 : 2;
+    }
 }
