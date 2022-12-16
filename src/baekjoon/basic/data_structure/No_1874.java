47bc9aba6ec92a72352009fb800a7c1c640f5be9
mr_kim <rofalfl42@gmail.com>
2022-12-16 23:41:40 +0900

    [BaekJoon] 백준 알고리즘 기초 1/2 자료구조 스택수열 풀이중

---
+++ src/baekjoon/basic/data_structure/No_1874.java
@@ -0,0 +1,27 @@
+package baekjoon.basic.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1874 {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+
+            int[] stackArr = new int[n];
+            for (int i = 0; i < n; i++) {
+                stackArr[i] = Integer.parseInt(br.readLine());
+            }
+            StringBuilder sb = new StringBuilder();
+
+            while (true) {
+                if ()
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
