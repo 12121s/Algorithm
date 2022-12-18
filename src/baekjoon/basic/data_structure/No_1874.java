096231514ebac02404d278eaf745f48230d3ab87
mr_kim <rofalfl42@gmail.com>
2022-12-18 22:07:25 +0900

    [BaekJoon] 백준 알고리즘 기초 1/2 자료구조 스택수열 완료

--- src/baekjoon/basic/data_structure/No_1874.java
+++ src/baekjoon/basic/data_structure/No_1874.java
@@ -3,6 +3,7 @@ package baekjoon.basic.data_structure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class No_1874 {
 
@@ -15,19 +16,30 @@ public class No_1874 {
             for (int i = 0; i < n; i++) {
                 stackArr[i] = Integer.parseInt(br.readLine());
             }
-            StringBuilder sb = new StringBuilder();
 
             int no = 1, x = 0;
-            while (x < stackArr.length) {
-                if (stackArr[x] == no) {
-                    sb.append("-\n");
+            StringBuilder sb = new StringBuilder();
+            Stack<Integer> stack = new Stack<>();
+            sb.append("+\n");
+            stack.push(no);
+            while (x < stackArr.length && no < stackArr.length + 1) {
+                if (!stack.isEmpty() && stackArr[x] == stack.peek()) {
+                    sb.append("-");
+                    stack.pop();
                     x++;
-                    no--;
+                    if (x != stackArr.length) sb.append("\n");
                 } else {
-                    sb.append("+\n");
-                    no++;
+                    sb.append("+");
+                    stack.push(++no);
+                    if (no != stackArr.length + 1) sb.append("\n");
                 }
             }
+            if (!stack.isEmpty()) {
+                sb = new StringBuilder();
+                sb.append("NO");
+            }
+
+            System.out.println(sb);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
