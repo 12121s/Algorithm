4870703ecb14278b9438173de34931a959bae441
mr_kim <rofalfl42@gmail.com>
2023-01-01 23:57:35 +0900

    [BaekJoon] 백준 단계별 문제 풀이 중 7단계(기본 수학, 7 풀이중)

---
+++ src/baekjoon/level07/No_2839.java
@@ -0,0 +1,18 @@
+package baekjoon.level07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class No_2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kg = sc.nextInt();
+        System.out.println(sugarBag(kg));
+    }
+    public static int sugarBag(int N) {
+        return 0;
+    }
+}
