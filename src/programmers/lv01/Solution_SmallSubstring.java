4debba702ec0f2afc8ce12f835934c83fc52047d
mr_kim <rofalfl42@gmail.com>
2023-01-02 14:14:15 +0900

    [Programmers] lv 01 - 크기가 작은 부분 문자열 풀이 완료

---
+++ src/programmers/lv01/Solution_SmallSubstring.java
@@ -0,0 +1,21 @@
+package programmers.intro;
+
+class Solution_FindingTheMode {
+    public static void main(String[] args) {
+        int[] sample01 = {1, 2, 3, 3, 3, 4};
+        int[] sample02 = {1, 1, 2, 2};
+        int[] sample03 = {1};
+
+        System.out.println(solution(sample01));
+        System.out.println(solution(sample02));
+        System.out.println(solution(sample03));
+
+    }
+
+    public static int solution(int[] array) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
