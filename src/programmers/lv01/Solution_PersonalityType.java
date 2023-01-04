af3e8959e46bfaae6a2d06970e10e3215d8d7549
mr_kim <rofalfl42@gmail.com>
2023-01-04 14:34:56 +0900

    [Programmers] lv 01 - 성격 유형 검사하기 풀이 완료

--- src/programmers/lv01/Solution_PersonalityType.java
+++ src/programmers/lv01/Solution_PersonalityType.java
@@ -1,5 +1,8 @@
 package programmers.lv01;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution_PersonalityType {
     public static void main(String[] args) {
         String[] sample1_1 = new String[] {"AN", "CF", "MJ", "RT", "NA"};
@@ -12,13 +15,28 @@ class Solution_PersonalityType {
     }
 
     public static String solution(String[] survey, int[] choices) {
-        String[][] type = {{"R", "T"}, {"C", "F"}, {"J", "M"}, {"A", "N"}};
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        Map<String, Integer> typeScore = new HashMap<>(Map.of(
+                "R", 0, "T", 0,
+                "C", 0, "F", 0,
+                "J", 0, "M", 0,
+                "A", 0, "N", 0));
 
         for (int i = 0; i < choices.length; i++) {
-
+            if (choices[i] < 4) { // 비동의 관련 선택 -> survey의 첫번째 캐릭터에 점수
+                typeScore.merge(survey[i].substring(0, 1), 4 - choices[i], Integer::sum);
+            } else { // 모르겠음 & 동의 관련 선택 -> survey의 두번째 캐릭터에 점수
+                typeScore.merge(survey[i].substring(1), choices[i] % 4, Integer::sum);
+            }
         }
 
-        return answer;
+        System.out.println(typeScore.toString());
+
+        answer.append(typeScore.get("R") >= typeScore.get("T")? "R": "T");
+        answer.append(typeScore.get("C") >= typeScore.get("F")? "C": "F");
+        answer.append(typeScore.get("J") >= typeScore.get("M")? "J": "M");
+        answer.append(typeScore.get("A") >= typeScore.get("N")? "A": "N");
+
+        return answer.toString();
     }
 }
