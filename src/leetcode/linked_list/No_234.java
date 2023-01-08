a970b1d239fab7a7c406f4dea7cf3c66c233cd15
mr_kim <rofalfl42@gmail.com>
2023-01-08 22:16:04 +0900

    [LeetCode 75 level02] Day 2 Linked List - Palindrome Linked List 문제 풀이 완료

--- src/leetcode/linked_list/No_234.java
+++ src/leetcode/linked_list/No_234.java
@@ -1,28 +1,54 @@
 package leetcode.linked_list;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class No_234 {
     public static void main(String[] args) {
         System.out.println(isPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))))));
         System.out.println(isPalindrome(new ListNode(1, new ListNode(2))));
         System.out.println(isPalindrome(new ListNode(1)));
         System.out.println(isPalindrome(new ListNode(1, new ListNode(0, new ListNode(1)))));
+        System.out.println(isPalindrome(new ListNode(0, new ListNode(1, new ListNode(1)))));
+        System.out.println(isPalindrome(new ListNode(1, new ListNode(1, new ListNode(0, new ListNode(1))))));
+        System.out.println(isPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(2, new ListNode(1)))))));
     }
 
     public static boolean isPalindrome(ListNode head) {
-        ListNode checkNode = head;
-        Set<Integer> checkSet = new HashSet<>();
+        ListNode endToMiddle = reverseList(middleNode(head));
 
-        while (checkNode != null) {
-            if (!checkSet.contains(checkNode.val))
-                checkSet.add(checkNode.val);
-            else checkSet.remove(checkNode.val);
+        while (head != null && endToMiddle != null) {
+            if (head.val != endToMiddle.val)
+                return false;
+            head = head.next;
+            endToMiddle = endToMiddle.next;
+        }
+        return true;
+    }
 
-            checkNode = checkNode.next;
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 갯수가 홀수인 경우
+        if (fast != null) slow = slow.next;
+
+        return slow;
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+
+        ListNode tempNode = new ListNode();
+        ListNode prevNode = null;
+
+        while(head != null) {
+            tempNode = head;
+            head = head.next;
+            tempNode.next = prevNode;
+            prevNode = tempNode;
         }
 
-        return checkSet.isEmpty();
+        return tempNode;
     }
 }
