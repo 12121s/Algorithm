63427762b767e542d890c88174f655d22ebc1885
mr_kim <rofalfl42@gmail.com>
2023-01-15 14:37:38 +0900

    [BaekJoon] 백준 단계별 문제 풀이 중 1단계(사칙연산 완료)

--- src/baekjoon/level01/ArithOperation.java
+++ src/baekjoon/level01/ArithOperation.java
@@ -3,6 +3,15 @@ package baekjoon.level01;
 import java.util.Scanner;
 public class ArithOperation {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a+b+c);
+    }
+
     public static void printResult() {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
