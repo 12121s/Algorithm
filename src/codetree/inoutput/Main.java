35de0a898f9a91ae196ac6b54ea72f2480268347
mr_kim <rofalfl42@gmail.com>
2023-01-22 01:50:54 +0900

    [CodeTree] 코드트리 기본 문법 (자바의 입출력) 복습 중 - 정수 입력

---
+++ src/codetree/inoutput/Main.java
@@ -0,0 +1,16 @@
+package codetree.inoutput;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* - 정수 입력 -
+        * 1. 입력받아 계산
+        * Scanner sc = new Scanner(System.in);
+        * int a = sc.nextInt();
+        * System.out.print(a + 2);
+        * */
+
+    }
+}
