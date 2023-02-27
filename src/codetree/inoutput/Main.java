73c6a1fb1c7231e0d585a135b98a4238201c39e8
mr_kim <rofalfl42@gmail.com>
2023-02-27 12:48:54 +0900

    [CodeTree] 코드트리 기본 문법 (자바의 입출력) 복습 중 - 날짜 변경하여 출력 *구분자가 . 이라면 앞에 \ 붙여주기*

--- src/codetree/inoutput/Main.java
+++ src/codetree/inoutput/Main.java
@@ -167,6 +167,15 @@ public class Main {
         * String birth = Integer.toString(sc.nextInt()); // 주민번호 앞
         * String special = Integer.toString(sc.nextInt()); // - 주민번호 뒤
         * System.out.println(birth + special);
+        *
+        * 4. 날짜 변경하여 출력
+        * Scanner sc = new Scanner(System.in);
+        * sc.useDelimiter("\\."); // - 기준으로 잘라서 입력 받겠다는 뜻
+        * int year = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        * int month = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        * int day = sc.nextInt(); // - 기호가 나오기 전 or 입력의 마지막 부분까지 진행
+        *
+        * System.out.println(month + "-" + day + "-" + year);
         * */
 
     }
