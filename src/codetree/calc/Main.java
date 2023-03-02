200bc49eadf622564921de2198520c81cda5fd28
mr_kim <rofalfl42@gmail.com>
2023-03-03 04:48:54 +0900

    [CodeTree] 코드트리 기본 문법 (사칙연산) 복습 중 - 두 수의 곱과 몫

--- src/codetree/calc/Main.java
+++ src/codetree/calc/Main.java
@@ -6,26 +6,34 @@ public class Main {
     public static void main(String[] args) {
         /* - 사칙연산 -
         * 1. 간단한 사칙연산
-        * Scanner sc = new Scanner(System.in);
-        * int a = sc.nextInt();
-        * int b = sc.nextInt();
-        *
-        * System.out.println(a + b);
-        * System.out.println(a - b);
-        * System.out.println(a / b);
-        * System.out.println(a % b);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a % b);
         *
         * 2. 길이 변형 후 사각형 넓이 구하기
-        * Scanner sc = new Scanner(System.in);
-        * int width = sc.nextInt() + 8;
-        * int height = sc.nextInt() * 3;
+        Scanner sc = new Scanner(System.in);
+        int width = sc.nextInt() + 8;
+        int height = sc.nextInt() * 3;
+
+        System.out.println(width);
+        System.out.println(height);
+        System.out.println(width * height);
         *
-        * System.out.println(width);
-        * System.out.println(height);
-        * System.out.println(width * height);
+        * 3. 두 수의 곱과 몫
+        *
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " * " + b + " = " + a * b);
+        System.out.println(a + " / " + b + " = " + a / b);
+        *
         * */
-
-
 
     }
 }
