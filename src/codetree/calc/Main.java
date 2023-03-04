11511192880fd4fb6675cde746b9309fdf2e4a85
mr_kim <rofalfl42@gmail.com>
2023-03-04 19:24:52 +0900

    [CodeTree] 코드트리 기본 문법 (사칙연산) 복습 중 - 몫과 나머지

--- src/codetree/calc/Main.java
+++ src/codetree/calc/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         /* - 사칙연산 -
         * 1. 간단한 사칙연산
+        * 
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
@@ -16,6 +17,7 @@ public class Main {
         System.out.println(a % b);
         *
         * 2. 길이 변형 후 사각형 넓이 구하기
+        *
         Scanner sc = new Scanner(System.in);
         int width = sc.nextInt() + 8;
         int height = sc.nextInt() * 3;
@@ -33,7 +35,17 @@ public class Main {
         System.out.println(a + " * " + b + " = " + a * b);
         System.out.println(a + " / " + b + " = " + a / b);
         *
+        * 4. 몫과 나머지
+        *
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a/b + "..." + a%b);
+        *
         * */
+
+
 
     }
 }
