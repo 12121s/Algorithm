daf65f3121c2c6988279e8aa72b023aadbe61b1c
mr_kim <rofalfl42@gmail.com>
2023-03-14 22:23:34 +0900

    [CodeTree] 코드트리 기본 문법 (조건문) 복습 중 - 음수 구별하기

---
+++ src/codetree/condition/Main.java
@@ -0,0 +1,19 @@
+package codetree.condition;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /* - if 조건문
+        * 1. 음수 구별하기
+        *
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println(a);
+        if (a < 0)
+            System.out.println("minus");
+        *
+        * */
+    }
+}
